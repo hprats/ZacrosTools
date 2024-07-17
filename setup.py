997b0153f53b67b8b763ada63eb1e7527fb36fba
hprats <hpratsgarcia@gmail.com>
2024-07-17 11:29:52 +0100

    Updated version

--- setup.py
+++ setup.py
@@ -5,15 +5,16 @@ with open("README.md", "r") as f:
 
 setup(
     name='zacrostools',
-    version='0.94-beta',
+    version='0.95-beta',
     description='A collective of tools for the preparation of input files for ZACROS',
     long_description=long_description,
     long_description_content_type='text/markdown',
     packages=find_packages(),
     url='https://github.com/hprats/ZacrosTools',
-    download_url='https://github.com/hprats/ZacrosTools/archive/refs/tags/v0.94-beta.tar.gz',
+    download_url='https://github.com/hprats/ZacrosTools/archive/refs/tags/v0.95-beta.tar.gz',
     author='Hector Prats',
     author_email='hpratsgarcia@gmail.com',
     keywords=['python', 'chemistry', 'KMC', 'ZACROS'],
     install_requires=['pandas', 'scipy']
 )
+x
