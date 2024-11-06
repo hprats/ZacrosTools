26e51aec26d948a92f1cd696ddcf0c0aa3786d5f
hprats <hpratsgarcia@gmail.com>
2024-11-06 19:24:47 +0000

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,33 +1,24 @@
 import os
+import sys
 from setuptools import setup, find_packages
+from zacrostools.version import __version__
+
+# Ensure the zacrostools package is accessible
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 
 
 with open("README.md", "r") as f:
     long_description = f.read()
 
-
-# Function to read the version from version.py
-def read_version():
-    version_path = os.path.join(os.path.dirname(__file__), 'zacrostools', 'version.py')
-    with open(version_path, 'r') as f:
-        for line in f:
-            if line.startswith('__version__'):
-                delim = '"' if '"' in line else "'"
-                return line.split(delim)[1]
-    raise RuntimeError("Unable to find version string.")
-
-
-version = read_version()
-
 setup(
     name='zacrostools',
-    version=version,
-    description='A collective of tools for the preparation of input files for ZACROS',
+    version=__version__,
+    description='A collection of tools for the preparation of input files for ZACROS',
     long_description=long_description,
     long_description_content_type='text/markdown',
     packages=find_packages(),
     url='https://github.com/hprats/ZacrosTools',
-    download_url=f'https://github.com/hprats/ZacrosTools/archive/refs/tags/v{version}.tar.gz',
+    download_url=f'https://github.com/hprats/ZacrosTools/archive/refs/tags/v{__version__}.tar.gz',
     author='Hector Prats',
     author_email='hpratsgarcia@gmail.com',
     keywords=['python', 'chemistry', 'KMC', 'ZACROS'],
