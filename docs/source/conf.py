22a1d29ed8700abafc8724c21be2777f00c888b1
hprats <hpratsgarcia@gmail.com>
2024-12-17 08:53:38 +0100

    Added logo to the documentation.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,3 +31,13 @@ exclude_patterns = []
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+# Add the following lines to set the logo
+html_logo = "../images/logo.png"  # Path relative to the _static directory
+
+# Optional: Customize theme options (if needed)
+html_theme_options = {
+    #'logo_only': True,  # Uncomment to display only the logo without the project name
+    #'display_version': True,  # Uncomment to hide the version in the sidebar
+    # Add more theme options here if desired
+}
